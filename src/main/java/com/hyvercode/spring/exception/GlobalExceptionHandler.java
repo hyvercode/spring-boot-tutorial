f532c796a59da7eae3815dcedc42b7fba385951e
Hyvercode <hyvercode@gmail.com>
2025-05-17 23:23:27 +0700

    API 500 exception handler

--- src/main/java/com/hyvercode/spring/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hyvercode/spring/exception/GlobalExceptionHandler.java
@@ -21,4 +21,14 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleOtherExceptions(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.put("error", "Internal Server Error");
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
